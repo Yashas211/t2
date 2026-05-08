8256d6dc65dbb3b31596c78798c0405444860bbe
Yashas211 <yashasbc21@gmail.com>
2026-05-08 14:22:00 +0530

    Update WebPageTest.java

--- src/test/java/WebPageTest.java
+++ src/test/java/WebPageTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -8,30 +9,43 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertTrue;
 
 public class WebPageTest {
+
     private static WebDriver driver;
 
     @BeforeTest
     public void openBrowser() throws InterruptedException {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        Thread.sleep(2000);
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+
         driver.get("https://yashas211.github.io/t2/");
+
+        Thread.sleep(2000);
     }
 
     @Test
-    public void titleValidationTest(){
-        String actualTitle = driver.getTitle();
-        String expectedTitle = "Department of ICB";
-        Assert.assertEquals(actualTitle, expectedTitle);
-        assertTrue(true, "Title should contain 'ICB'");
+    public void titleValidationTest() {
 
+        String actualTitle = driver.getTitle();
+
+        String expectedTitle = "Department of ICB";
+
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+        assertTrue(actualTitle.contains("ICB"),
+                "Title should contain 'ICB'");
     }
 
     @AfterTest
     public void closeBrowser() throws InterruptedException {
-        Thread.sleep(10000);
+
+        Thread.sleep(2000);
+
         driver.quit();
     }
-
-
 }
